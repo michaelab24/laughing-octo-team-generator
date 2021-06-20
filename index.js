88fba35f1a06a0892087f94479999eb5b4be6f56
michaelab24 <michaelbarnes459@yahoo.com>
2021-06-20 09:35:58 -0500

    could be messing it up

---
+++ index.js
@@ -0,0 +1,13 @@
+const inquirer = require("inquirer");
+const fs = require("fs")
+const generateHTML = require("./src/generateHTML")
+
+const questions = [
+    {
+
+        type: 'input',
+        message: 'What is the description of your project? (*Please enter the links to your github and project here.*)',
+        name: 'description'
+    },
+
+]
