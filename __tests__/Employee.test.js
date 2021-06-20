cf1ed4b25cfe6310d141eecd2f8ca68529ec3d1f
michaelab24 <michaelbarnes459@yahoo.com>
2021-06-20 09:04:44 -0500

    deleted some code

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,7 +3,7 @@ const Employee = require('../lib/Employee');
 
 // creates employee object 
 test('creates an employee object', () => {
-    const employee = new Employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const employee = new Employee();
 
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
@@ -12,14 +12,14 @@ test('creates an employee object', () => {
 
 // gets id from getId() 
 test('gets employee name', () => {
-    const employee = new Employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const employee = new Employee();
 
     expect(employee.getName()).toEqual(expect.any(String));
 });
 
 // gets id from getId() 
 test('gets employee ID', () => {
-    const employee = new Employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const employee = new Employee();
 
     expect(employee.getId()).toEqual(expect.any(Number));
 });
