68839a79fd1838c3f2ab679f9db7703b125e2d74
michaelab24 <michaelbarnes459@yahoo.com>
2021-06-22 15:31:27 -0500

    all the files again

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const generateHTML = require("./src/generateHTML")
 
 const questions = [
     {
+        type: 'input',
+        message: 'What is the description of your project? (*Please enter the links to your github and project here.*)',
+        name: 'description'
     },
 
 ]
