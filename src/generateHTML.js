08d717a803683f8d5e7ea1324018bb171ab9dcab
michaelab24 <michaelbarnes459@yahoo.com>
2021-06-24 23:27:23 -0500

    well it's something

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,7 +1,21 @@
-const generateHTML = (team) => {
-    let html = 'This will be where the html will be'
-    console.log(team)
-    return html
+const generateHTML = function (createTeam) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+         <script src="index.js"></script>
+         <script src="generateHTML.js"></script>
+        <title>Team Generator</title>
+    </head>
+    <body>
+        <h1>My Team</h1>
+    </body>
+    </html>` 
 }
-
+//get array through the team
+//for each team member pull out objects of array
+//slap em to a cards
 module.exports = generateHTML;
