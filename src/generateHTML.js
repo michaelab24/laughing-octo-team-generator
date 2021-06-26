dbca237be1cd759431a7a18205780e7166b82e6a
michaelab24 <michaelbarnes459@yahoo.com>
2021-06-25 22:19:10 -0500

    some styling

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -14,31 +14,34 @@ const generateHTML = function (createTeam) {
     <body>
     <h1>My Team</h1>
 
-    <div class="row">
-        <div id="cards" class="teamCards">
-            <div id="manager" class="bossMan">
-                <div>Name: <span id="name1"></span></div>
-                <div>Employee ID: <span id="id1"></span></div>
-                <div>Email: <span id="email1"></span></div>
-                <div>Office Number: <span id="num1"></span></div>
-            </div>
-            <div id="cards" class="teamCards">
-                <div id="engineer" class="codeMan">
-                    <div>Name: <span id="name2"></span></div>
-                    <div>Employee ID: <span id="id2"></span></div>
-                    <div>Email: <span id="email2"></span></div>
-                    <div>gitHub: <span id="gitHub"></span></div>
-                </div>
-            </div>
-            <div id="cards" class="teamCards">
-                <div id="intern" class="lowMan">
-                    <div>Name: <span id="name3"></span></div>
-                    <div>Email: <span id="email3"></span></div>
-                    <div>School: <span id="school"></span></div>
+    <div id="cards" class="teamCards">
+        <div id="manager" class="bossMan">
+            <h2>Manager</h2>
+            <div>Name: <span id="name1"></span></div>
+            <div>Employee ID: <span id="id1"></span></div>
+            <div>Email: <span id="email1"></span></div>
+            <div>Office Number: <span id="num1"></span></div>
+    </div>
+    <div id="cards" class="teamCards">
+        <div id="engineer" class="codeMan">
+            <h2>Engineer</h2>
+            <div>Name: <span id="name2"></span></div>
+            <div>Employee ID: <span id="id2"></span></div>
+            <div>Email: <span id="email2"></span></div>
+            <div>gitHub: <span id="gitHub"></span></div>
+        </div>
+    </div>
+    <div id="cards" class="teamCards">
+        <div id="intern" class="lowMan">
+            <h2>Intern</h2>
+            <div>Name: <span id="name3"></span></div>
+            <div>Email: <span id="email3"></span></div>
+            <div>School: <span id="school"></span></div>an></div>
                 </div>
             </div>
         </div>
     </div>
+<div/>
 
 
 
